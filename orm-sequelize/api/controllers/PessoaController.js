fc01f38c91ec9561dc22a6af0fb05162510d24ce
luisaoli <luisaoliveira.e@gmail.com>
2022-04-26 11:41:36 -0300

    soft delete em matriculas, turmas e niveis

--- orm-sequelize/api/controllers/PessoaController.js
+++ orm-sequelize/api/controllers/PessoaController.js
@@ -105,6 +105,16 @@ class PessoaController {
         }
     }
 
+    static async restauraMatricula(req, res) {
+        const { estudanteId, matriculaId } = req.params;
+        try {
+            await database.Matriculas.restore( { where: { id: Number(matriculaId), estudante_id: Number(estudanteId) } } )
+            return res.status(200).json({ message: `id ${matriculaId} restaurado`})
+        } catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
 }
 
 module.exports = PessoaController;
--- orm-sequelize/api/routes/pessoasRoute.js
+++ orm-sequelize/api/routes/pessoasRoute.js
@@ -11,6 +11,7 @@ router.put('/pessoas/:id', PessoaController.atualizaPessoa);
 router.delete('/pessoas/:id', PessoaController.apagaPessoa);
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
 router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
+router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula);
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula);
 
